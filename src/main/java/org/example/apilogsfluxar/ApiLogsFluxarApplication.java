ac068a7d04e28f4c390fb60da9538d9a5b2f9119
Natália Trindade <natalia.santos@germinare.org.br>
2025-09-08 08:06:21 -0300

    feat: iniciar projeto spring boot

---
+++ src/main/java/org/example/apilogsfluxar/ApiLogsFluxarApplication.java
@@ -0,0 +1,13 @@
+package org.example.apilogsfluxar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLogsFluxarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLogsFluxarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/apilogsfluxar/ApiLogsFluxarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apilogsfluxar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLogsFluxarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
