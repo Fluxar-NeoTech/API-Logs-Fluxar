e6e2c550315ec977e4bc7ce7dfbaceed627b6e04
Natália Trindade <natalia.santos@germinare.org.br>
2025-10-01 07:10:20 -0300

    feat: adicionar metodo de consultar por data da ação

--- src/main/java/com/example/api_logs_fluxar/repository/UserLogRepository.java
+++ src/main/java/com/example/api_logs_fluxar/repository/UserLogRepository.java
@@ -4,7 +4,7 @@ import com.example.api_logs_fluxar.model.UserLog;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface UserLogRepository extends MongoRepository<UserLog, String> {
@@ -12,4 +12,7 @@ public interface UserLogRepository extends MongoRepository<UserLog, String> {
     List<UserLog> findByUser_id(Long user_id);
 
     List<UserLog> findByAction(String action);
+
+    @Query("{ 'done_at': ?0 }")
+    List<UserLog> findByDone_at(LocalDate date);
 }
